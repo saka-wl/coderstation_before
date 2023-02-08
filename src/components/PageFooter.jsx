2745d76f7c73234349e8b044828f2f7c1c08f279
wl王立 <aywzcmq64@163.com>
2023-02-08 09:52:00 +0800

    start

---
+++ src/components/PageFooter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PageFooter(props) {
+    return (
+        <div>
+            <p>© 2023 - Coder Station - Saka-wlll</p>
+            <p>Powered by Create React App</p>
+        </div>
+    );
+}
+
+export default PageFooter;
